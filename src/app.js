a523c33c5d6b5d15e3e1c869a037ba33b5e7a843
Soumik Paul <soumik.paul@contentstack.com>
2023-02-01 20:54:25 +0530

    Minor fix

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ import { About } from "./components/About.js";
 
 const root = document.getElementById("root");
 
-let API_URL = `${window.location.protocol}//${window.location.host}/src/tasks/personal-website/`;
-if (window.location.host === "soumik43.github.io/") {
-    API_URL = window.location;
-}
-
 const components = [Header, About];
 
 components.forEach((comp) => {
